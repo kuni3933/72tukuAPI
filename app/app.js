5c595f084d1b418375730dfbdf7e666edd4cfd9e
kuni3933 <57650186+kuni3933@users.noreply.github.com>
2022-01-18 16:36:48 +0900

    [Update] ファビコンの処理を追加
    Signed-off-by: kuni3933 <57650186+kuni3933@users.noreply.github.com>

--- app/app.js
+++ app/app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const router = express.Router();
+
+const favicon = require("serve-favicon");
 
 const cors = require("cors");
 
@@ -15,6 +16,14 @@ app.use(express.json());
 // CORS設定。異なるURLからでも呼び出せるようにする
 app.use(cors());
 
+// ファビコン設定
+app.use(
+  favicon(
+    __dirname + "/" + "nanichan_icon1.ico",
+    (maxAge = 1000 * 60 * 60 * 24 * 30)
+  )
+);
+
 // "app/v1"を定数v1に読み込む
 const v1 = require("./v1/");
 
@@ -24,14 +33,14 @@ app.use("/v1/", v1);
 // http://future-is-now-k02.blogspot.com/2012/11/nodejs-expressurl404.html
 //エラーハンドリング(404)
 app.use(function (req, res, next) {
-  res.status(404).json({ Error: "Error" });
+  res.status(404).json(JSON.stringify({ status: 0, Error: "Error" }));
 });
 
 //todo
 // エラーハンドリング(500)
 process.on("uncaughtException", (err) => {
   console.log(err);
-  res.status(500).json(JSON.stringify({ Error: "Error" }));
+  res.status(500).json(JSON.stringify({ status: 0, Error: "Error" }));
 });
 
 // 環境変数PORT || 3000 をポート番号に指定
